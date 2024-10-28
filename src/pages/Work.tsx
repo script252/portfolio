09c0e65a39a1e4c74b87d82c03c7359e923a4ebd
Andrew Closson <script252@gmail.com>
2024-10-28 11:07:41 -0500

    Work page cards and responsiveness.

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,7 +1,56 @@
 function Work() {
     return (
       <>
-        <h1>Work</h1>
+        <div className="px-0 lg:px-8">
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+            <div className="card bg-p-metal-900 rounded-none w-full lg:w-90 xl:w-96">
+              <figure className="">
+                <img
+                  className=""
+                  src="assets/placeholder-screenshot.jpg"
+                  alt="Screenshot" />
+              </figure>
+              <div className="card-body px-8 pt-8 pb-4">
+                <h2 className="card-title text-p-tan-light-700">Dealforce</h2>
+                <p>A marketplace app to shop for businesses.</p>
+                <div className="card-actions justify-end">
+                <div className="badge pt-1 text-p-tan-light-700 bg-p-tan-light-950">React</div>
+                <div className="badge pt-1 text-p-tan-light-700 bg-p-tan-light-950">Next</div>
+                </div>
+              </div>
+            </div>
+            <div className="card bg-p-metal-900 rounded-none w-full lg:w-90 xl:w-96">
+              <figure>
+                <img
+                  src="assets/placeholder-screenshot.jpg"
+                  alt="Screenshot" />
+              </figure>
+              <div className="card-body px-8 pt-8 pb-4">
+                <h2 className="card-title text-p-tan-light-700">Hero</h2>
+                <p>Internal safety incident reporting tool created for Johns Hopkins.</p>
+                <div className="card-actions justify-end">
+                <div className="badge pt-1 text-p-tan-light-700 bg-p-tan-light-950">Angular</div>
+                </div>
+              </div>
+            </div>
+            <div className="card bg-p-metal-900 rounded-none w-full lg:w-90 xl:w-96">
+              <figure>
+                <img
+                  src="assets/placeholder-screenshot.jpg"
+                  alt="Screenshot" />
+              </figure>
+              <div className="card-body px-8 pt-8 pb-4">
+                <h2 className="card-title text-p-tan-light-700">CAMS</h2>
+                <p className="">Internal administration tool created for Crash Champions.</p>
+                <div className="card-actions justify-end pt-4">
+                  <div className="badge pt-1 text-p-tan-light-700 bg-p-tan-light-950">React</div>
+                  <div className="badge pt-1 text-p-tan-light-700 bg-p-tan-light-950">Tailwind CSS</div>
+                  <div className="badge pt-1 text-p-tan-light-700 bg-p-tan-light-950">Tanstack</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </>
     )
   }
