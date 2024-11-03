be23da4abcb91bc5f60a14218c8e122d9f528d44
Andrew Closson <script252@gmail.com>
2024-11-03 00:53:33 -0500

    Updates

---
+++ assets/404-73b97c22.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-2733acc9.js";import{H as s}from"./Head-c2bcb40f.js";function n(){return e.jsxs(e.Fragment,{children:[e.jsx(s,{title:"The page is not found"}),e.jsxs("div",{className:"flex flex-col items-center",children:[e.jsx("h1",{children:"404"}),e.jsx("h3",{children:"Not found"})]})]})}export{n as default};
